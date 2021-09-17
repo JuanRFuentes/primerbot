f165014e6f6ab63b556b8adf3c45202dc714ebda
Juan <90003192+JuanRFuentes@users.noreply.github.com>
2021-09-17 07:49:37 -0500

    Update Vico.js

--- Vico.js
+++ Vico.js
@@ -124,7 +124,23 @@ const button = {
 }
 
 client . sendMessage ( from, button, MessageType.listMessage)
-	
+	// send a buttons message!
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1}
+]
+
+const buttonMessage = {
+    contentText: "Hi it's button message",
+    footerText: 'Hello World',
+    buttons: buttons,
+    headerType: 1
+}
+
+client . sendMessage(from, buttonMessage, MessageType.buttonsMessage)
+const conpa = fs.readFileSync('./Media/User.vcf')
+client.sendMessage(from, conpa, Mimetype.vcf,
+)
 }
 	
 	
